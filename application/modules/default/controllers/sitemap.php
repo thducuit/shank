67b5e062e1a4c8aaef21054a47bb05fe29da918b
thducuit <thducuit@gmail.com>
2015-12-23 08:35:48 +0700

    fix

--- application/modules/default/controllers/sitemap.php
+++ application/modules/default/controllers/sitemap.php
@@ -4,18 +4,19 @@ require_once APPPATH . 'modules/default/controllers/parent.php';
 
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 class Sitemap extends Parent_Controller {
-
+	private $data;
 	 function __construct() {
         //if i remove this parent::__construct(); the error is gone
         parent::__construct();
+        $this->data = $this->get_data();
 	}
 	
 	public function index() {
 	    //RUN VIEW
-        $this->template->build( 'sitemap/index' );
+        $this->template->build( 'sitemap/index', $this->data );
 	}
 	
-	public function test($lang) {
-	    _pr($lang, true);
-	}
+	// public function test($lang) {
+	//     _pr($lang, true);
+	// }
 }
